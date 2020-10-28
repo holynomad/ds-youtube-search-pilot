cb79a204e1cf53d70f7f5edd6c9002ea212b6432
hismacgyver <daniel81.lee@gmail.com>
2020-10-28 12:39:44 +0900

    it's 1st commit

---
+++ search.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+  var API_KEY = ""
+  var video = ""
+  var videos = $("#videos")
+
+  $("#form").submit(function(event){
+    event.preventDefault();
+    //alert("form is submitted");
+
+    var search = $("#search").val();
+
+    videoSearch(API_KEY, search, 6);
+  })
+  
+
+  function videoSearch(key, search, maxResults){
+
+    $("#videos").empty()
+
+    $.get("https://www.googleapis.com/youtube/v3/search?key="+key
+    + "&type=video&part=snippet&maxResults="+maxResults+ "&q="+search, function(data){
+      console.log(data)
+
+      data.items.forEach(item => {
+        video = `
+          <iframe src="https://www.youtube.com/embed/${item.id.videoId}" height="315" width="420" frameboarder="0" allowfullscreen></iframe>
+        `
+
+        $("#videos").append(video)
+      });
+    })
+      
+  }
+})
