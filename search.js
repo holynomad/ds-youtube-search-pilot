ca2e93fbdd57a612d0c93b2d26315e8f3c47cf68
hismacgyver <daniel81.lee@gmail.com>
2020-11-21 17:33:24 +0900

    add table format for crawling contents

--- search.js
+++ search.js
@@ -85,63 +85,32 @@ $(document).ready(function(){
         console.log('2-1', index)
         console.log(data[index])
         
+        // 표 형식 추가 @ 2020.11.21
+        // 참조 : https://blog.naver.com/neo_start/220322362255
         video = `
-          <iframe src="https://www.youtube.com/embed/${item.id.videoId}" height="315" width="420" frameboarder="0" allowfullscreen></iframe>
-          <h6>ㅁ 채널ID : ${item.snippet.channelId}</h6>
-          <h6>ㅁ 채널명 : ${item.snippet.channelTitle}</h6>
-          <h6>ㅁ 제목 : ${item.snippet.title}</h6>
-          <h6>ㅁ Desc. : ${item.snippet.description}</h6>
-          <h6>ㅁ 게시일시 : ${item.snippet.publishedAt}</h6> 
-        `
-
-        // test
-        /*
-        videoString = `
-          "https://www.youtube.com/embed/${item.id.videoId}, ${item.snippet.channelId}, ${item.snippet.channelTitle}"
-        ` 
-        */       
-
-        
-        //console.log(Array.isArray(videoString))
-        //console.log(videoString)
-        
-
-        // videoDetails로 일원화 주석 @ 2020.11.14.
-        //$("#videos").append(video);
-
-        //console.log('2-2', data[index].id.videoId, data[index].snippet.channelId, video)
-
+            <tr>
+              <td>
+                <iframe src="https://www.youtube.com/embed/${item.id.videoId}" height="315" width="420" frameboarder="0" allowfullscreen></iframe>
+              </td>
+              <td>
+                <h6>${item.snippet.channelId}</h6>
+              </td>
+              <td>
+                <h6>${item.snippet.channelTitle}</h6>
+              </td>
+              <td>
+                <h6>${item.snippet.title}</h6>
+              </td>
+              <td>
+                <h6>${item.snippet.description}</h6>
+              </td>
+              <td>
+                <h6>${item.snippet.publishedAt}</h6>   
+              </td>
+        `    
+        // video 상세정보 조회 + 추가한 다음, tbody에 표 형식으로 추가
         videoDetails(API_KEY, data[index].id.videoId, data[index].snippet.channelId, video);
         
-        //document.getElementsByName('details')[0] = data[index].id.videoId;
-
-        //document.getElementById("details").value = data[index].id.videoId;
-
-        //$.get("https://www.googleapis.com/youtube/v3/videos?id="+item.id.videoId + "&key="+key
-        //+ "&part=snippet,contentDetails,statistics,status", function(detail){
-          
-
-          //$("#details").append("totalResults : " + detail.snippet.tags[])
-          
-        //  detail.items.forEach(function(item, index_2, det) {
-        //    console.log('3-1', index_2)
-        //    console.log(det[index_2])
-
-        //    if (index == index_2 && snippet.channelId == item.snippet.channelId) {
-        //      alert(det[index_2].contentDetails.duration)
-              
-              
-              //$("#contents").append("<br>contentDetails.duration : " + det[index_2].contentDetails.duration);
-              //$("#contents").append("<br>statistics.viewCount : " + det[index_2].statistics.viewCount);
-              //$("#contents").append("<br>statistics.commentCount : " + det[index_2].statistics.commentCount);
-              //$("#contents").append("<br>statistics.favoriteCount : " + det[index_2].statistics.favoriteCount);
-              //$("#contents").append("<br>snippet.tags : " + det[index_2].snippet.tags);
-        //    }
-        //  })    
-          
-        //  $("#contents").append(details);
-
-        //})
 
       });
     })
@@ -154,8 +123,6 @@ $(document).ready(function(){
 
     $("#contents").empty()
 
-
-
     $.get("https://www.googleapis.com/youtube/v3/videos?id="+videoId + "&key="+key
     + "&part=snippet,contentDetails,statistics,status", function(detail){
       console.log("called getting videoDetails")  
@@ -167,22 +134,20 @@ $(document).ready(function(){
         if (channelId == det[index].snippet.channelId) {
           
           videoDetail = videoinfo + `
-            <h6> ㅁ 재생시간 : ${det[index].contentDetails.duration} </h6>
-            <h6> ㅁ 조회수 : ${det[index].statistics.viewCount} </h6>
-            <h6> ㅁ 댓글수 : ${det[index].statistics.commentCount} </h6>
-            <h6> ㅁ Tags : ${det[index].snippet.tags} </h6>
+              <td>
+                <h6>${det[index].contentDetails.duration}</h6>
+              </td>
+              <td>
+                <h6>${det[index].statistics.viewCount} </h6>
+              </td>
+              <td>
+                <h6>${det[index].statistics.commentCount} </h6>
+              </td>
+              <td>
+                <h6>${det[index].snippet.tags} </h6>
+              </td>
+            </tr>
           `
-          
-
-          // test
-          /*
-          videoDetail = videoinfo.split(',')
-          console.log(Array.isArray(videoDetail))
-          console.log(videoDetail)
-          */
-
-          // 구독자수는 channels api 통해 추가작업 필요
-          //<h6> ㅁ 구독자수 : ${det[index].statistics.subscriberCount} </h6>
 
           $("#videos").append(videoDetail);
 
